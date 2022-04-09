bfafc2791acc0846c7808354778bcd18e7b353f5
Yoshi971 <101185810+Yoshi971@users.noreply.github.com>
2022-04-09 21:00:23 +0200

    exercise 6: Extracting subjects and objects of the sentence

--- exercise6_Extracting_subjects_and_objects_of_the_sentence.py
+++ exercise6_Extracting_subjects_and_objects_of_the_sentence.py
@@ -27,12 +27,36 @@ def get_object_phrase(doc):
             start = subtree[0].i
             end = subtree[-1].i + 1
             return doc[start:end]
+            
+#The dative object function
+def get_dative_phrase(doc):
+    for token in doc:
+        if ("dative" in token.dep_):
+            subtree = list(token.subtree)
+            start = subtree[0].i
+            end = subtree[-1].i + 1
+            return doc[start:end]
+            
+#prepositional object function
+def get_prepositional_phrase_objs(doc):
+    prep_spans = []
+    for token in doc:
+        if ("pobj" in token.dep_):
+            subtree = list(token.subtree)
+            start = subtree[0].i
+            end = subtree[-1].i + 1
+            prep_spans.append(doc[start:end])
+    return prep_spans
 
 print("loop through the sentences and print out their subjects and objects")
 for sentence in sentences:
     doc = nlp(sentence)
     subject_phrase = get_subject_phrase(doc)
     object_phrase = get_object_phrase(doc)
+    dative_phrase = get_dative_phrase(doc)
+    prepositional_phrase = get_prepositional_phrase_objs(doc)
     print(subject_phrase)
     print(object_phrase)
+    print(dative_phrase)
+    print(prepositional_phrase)
     
